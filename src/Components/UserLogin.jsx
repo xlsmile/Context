8143a6a3c189acbfdb53c13f749060e4e4fc7bc7
Serge Seva <sergeseva@catalyst.net.nz>
2022-11-22 11:44:40 +1300

    Initial commit

--- src/Components/UserLogin.jsx
+++ src/Components/UserLogin.jsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
+
 const UserLogin = () => {
+
+  const [userName, setUserName] = useState("");
+  const [showProfile, setShowProfile] = useState(false);
+
+  const handleUserName = (e) => {
+    setUserName(e.target.value);
+  }
+
+  const handleShowProfile = () => {
+    setShowProfile(true);
+  }
+
   return (
     <>
       <div>
         <label className="form__label" htmlFor="username">User Name</label>
-        <input className="form__input" id="username" type="text" />
+        <input className="form__input" id="username" type="text" onChange={handleUserName}/>
       </div>
       <div>
         <label  className="form__label" htmlFor="userpassword">User Password</label>
         <input  className="form__input" id="userpassword" type="password" />
       </div>
       <div className="form__group--buttons">
-        <button className="cta cta--success">Login</button>
+        <button className="cta cta--success" onClick={handleShowProfile}>Login</button>
+      </div>
+      <div>
+        {showProfile && <p className="text">{userName}</p>}
       </div>
     </>
   )
