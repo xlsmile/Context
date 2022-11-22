3764285d076a11830901c08841640bdba79bd781
Serge Seva <sergeseva@catalyst.net.nz>
2022-11-23 10:05:41 +1300

    Restructure app.

---
+++ src/Components/User/UserProfile.jsx
@@ -0,0 +1,17 @@
+import {useContext} from "react";
+import {LoginContext} from "../Context/LoginContext";
+
+const UserProfile = () => {
+
+  const {userName, setShowProfile} = useContext(LoginContext);
+
+  return (
+    <>
+      <h2 className="title__secondary">User Profile</h2>
+      <h3 className="title__tertiary">User Name: {userName}</h3>
+      <button className="cta cta--start cta__back" onClick={() => setShowProfile(false)}><i className="fa-solid fa-chevron-left fa-xs"></i>Back</button>
+    </>
+  )
+}
+
+export default UserProfile;
